4007720e851694ceda76466a3d83c5510148dcb8
Szymon Jankowski <s.jankowski@fireup.pro>
2021-02-27 13:32:42 +0100

    Injecting parameters to methods

--- src/scraper/util/__tests__/parameter-injector.test.ts
+++ src/scraper/util/__tests__/parameter-injector.test.ts
@@ -1,36 +1,135 @@
-// import {
-//   getMethodInjectedParams,
-//   InjectA,
-// } from "../../../util/parameter-injector";
-// import { RootScopeContext } from "../../scope/root-scope-context";
-// import { ScopeContext } from "../../scope/scope-context";
-// import { FeatureConfiguration } from "../configuration";
-// import { Feature } from "../feature-class";
+import {
+  getMethodInjectedParams,
+  InjectParam,
+  invokeAndInjectParams,
+} from "../parameter-injector";
+
+describe("Parameter injector tests", () => {
+  function InjectA(): ParameterDecorator {
+    return InjectParam("param-a");
+  }
+
+  function InjectB(): ParameterDecorator {
+    return InjectParam("param-b");
+  }
+
+  test("Should get info about injected params", () => {
+    class A {
+      func(
+        @InjectA() asd: string,
+        nonInjected: boolean,
+        @InjectB() asdf: number
+      ) {}
+    }
+
+    const injectedParams = getMethodInjectedParams(A.prototype, "func");
+
+    expect(injectedParams).toEqual(
+      expect.arrayContaining([
+        { type: "param-a", index: 0 },
+        { type: "param-b", index: 2 },
+      ])
+    );
+  });
+
+  describe("Function invocation", () => {
+    class A {
+      field = 123;
+
+      func(@InjectA() a: number) {
+        return [...arguments];
+      }
+
+      func2(
+        param1: number,
+        @InjectA() param2: number,
+        @InjectB() param3: string,
+        param4: string
+      ) {
+        return [...arguments];
+      }
+
+      func3(@InjectB() b: string) {
+        return [this.field, b];
+      }
+    }
+
+    const a = new A();
+
+    test("Should inject undefined if parameter not provided", () => {
+      const result = invokeAndInjectParams(a, "func", {}, []);
+      expect(result).toEqual([undefined]);
+    });
+
+    test("Should inject parameter if provided", () => {
+      const result = invokeAndInjectParams(
+        a,
+        "func",
+        {
+          "param-a": 123,
+        },
+        []
+      );
+      expect(result).toEqual([123]);
+    });
+
+    test("Should inject multiple parameters and preserve other params", () => {
+      const result = invokeAndInjectParams(
+        a,
+        "func2",
+        {
+          "param-a": 123,
+          "param-b": "injected",
+        },
+        [1, undefined, "should be ignored", "test"]
+      );
+      expect(result).toEqual([1, 123, "injected", "test"]);
+    });
+
+    test("Should preserve this", () => {
+      a.field = 321;
+      const result = invokeAndInjectParams(
+        a,
+        "func3",
+        {
+          "param-b": "injected",
+        },
+        []
+      );
+      expect(result).toEqual([321, "injected"]);
+    });
+  });
+});
+
+//// Code related to function parameter exclusion for feature classes
 //
-// describe("Parameter injector tests", () => {
-//   test("Test", () => {
-//     class A {
-//       func(@InjectA() asd: string) {}
-//     }
+// function RootScope() {}
 //
-//     console.log(getMethodInjectedParams(A.prototype, "func"));
-//   });
-// });
+// function Config() {}
 //
 // class Feat extends Feature {
 //   method(
-//     @Scope() scope: ScopeContext,
-//     @RootScope() root: RootScopeContext,
+//     @InjectA() scope: ScopeContext,
+//     @InjectA() root: RootScopeContext,
 //     a: number,
 //     b: number
 //   ) {}
 //
-//   init_test(@Config() config: FeatureConfiguration, a: string) {}
+//   init_test(@InjectA() config: FeatureConfiguration, a: string) {}
 // }
 //
-// type ExcludeInjectableTypes<T extends unknown[]> = T extends [...infer Params, infer A]
-//   // @ts-ignore Type instantiation is excessively deep and possibly infinite.(2589) - This problem is not present in TS 4.2
-//   ? [...ExcludeInjectableTypes<Params>, ...(A extends ScopeContext ? [] : (T extends [...Params, ...infer R] ? R : []))]
+// type ExcludeInjectableTypes<T extends unknown[]> = T extends [
+//   ...infer Params,
+//   infer A
+// ]
+//   ? [
+//       ...ExcludeInjectableTypes<Params>,
+//       ...(A extends ScopeContext
+//         ? []
+//         : T extends [...Params, ...infer R]
+//         ? R
+//         : [])
+//     ]
 //   : T;
 //
 // type FunctionWithoutInjectable<T> = T extends (
@@ -43,9 +142,9 @@
 //   [param1: string, param2: ScopeContext, param3: number]
 // >;
 // type A = FunctionWithoutInjectable<Feat["method"]>;
-//
+
 // // Scope usage
 // scope.feature(Feat).method(2, 3);
-//
-// // Init usage
-// run.feature(Feat).test("asd");
+// //
+// // // Init usage
+// // run.feature(Feat).test("asd");
--- src/scraper/util/parameter-injector.ts
+++ src/scraper/util/parameter-injector.ts
@@ -1,30 +1,65 @@
 // TypeScript 4.1 won't allow me to do magic with function mapping - I can't ignore parameters of specific type
 // on a function. Fortunately TS 4.2 makes it possible, but we have to wait for release.
+// TODO: Update - TS 4.2 was released :)
 
-// import "reflect-metadata";
-// import { addMethodMetadata } from "../robot/metadata-helpers";
-//
-// const INJECTED_PARAMS_KEY = Symbol("InjectedParams");
-//
-// export type InjectedParamMetadata = [index: number, type: string];
-//
-// export function InjectA(): ParameterDecorator {
-//   return InjectParam("a");
-// }
-//
-// export function InjectParam(type: string): ParameterDecorator {
-//   return (target, propertyKey, parameterIndex) => {
-//     const metadata = {
-//       type,
-//       index: parameterIndex,
-//     };
-//     addMethodMetadata(INJECTED_PARAMS_KEY, target, propertyKey, metadata);
-//   };
-// }
-//
-// export function getMethodInjectedParams(
-//   target: any,
-//   methodName: string
-// ): InjectedParamMetadata[] {
-//   return Reflect.getOwnMetadata(INJECTED_PARAMS_KEY, target, methodName) ?? [];
-// }
+import "reflect-metadata";
+import { addMethodMetadata } from "../robot/metadata-helpers";
+
+const INJECTED_PARAMS_KEY = Symbol("InjectedParams");
+
+export type InjectedParamMetadata = { index: number; type: string };
+
+export function InjectParam(type: string): ParameterDecorator {
+  return (target, propertyKey, parameterIndex) => {
+    const metadata = {
+      type,
+      index: parameterIndex,
+    };
+    addMethodMetadata(INJECTED_PARAMS_KEY, target, propertyKey, metadata);
+  };
+}
+
+export function getMethodInjectedParams(
+  target: any,
+  methodName: string | symbol
+): InjectedParamMetadata[] {
+  return Reflect.getOwnMetadata(INJECTED_PARAMS_KEY, target, methodName) ?? [];
+}
+
+type ClassMethods<T> = {
+  [K in keyof T as T[K] extends (...params: infer Params) => infer Return
+    ? K
+    : never]: T[K];
+};
+
+// This type was borrowed from jest's spyOn method, I couldn't get MethodName to be a valid class index
+type ClassMethodNames<T> = {
+  [K in keyof T]: T[K] extends (...args: any[]) => any ? K : never;
+}[keyof T] &
+  string;
+
+export function invokeAndInjectParams<
+  TClass extends {},
+  MethodName extends ClassMethodNames<TClass>
+>(
+  thisArg: TClass & ClassMethods<TClass>,
+  methodName: MethodName,
+  providers: Record<string, any>,
+  params: TClass[MethodName] extends (...args: infer Params) => any
+    ? Partial<Params>
+    : never
+): TClass[MethodName] extends (...args: any[]) => any
+  ? ReturnType<TClass[MethodName]>
+  : never {
+  const method: any = thisArg[methodName];
+  const paramsMetadata = getMethodInjectedParams(
+    Object.getPrototypeOf(thisArg),
+    methodName as string
+  );
+
+  for (const { index, type } of paramsMetadata) {
+    params[index] = providers[type];
+  }
+
+  return method.apply(thisArg, params);
+}
