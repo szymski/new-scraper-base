796bb6420daaebda26194216d5600aa342f56a20
Szymon Jankowski <s.jankowski@fireup.pro>
2021-03-06 17:33:16 +0100

    Cookie boostraping

--- src/scraper/http-client/interceptors/persist-cookie.interceptor.ts
+++ src/scraper/http-client/interceptors/persist-cookie.interceptor.ts
@@ -3,20 +3,34 @@ import { RequestInterceptor, ResponseInterceptor } from "./interfaces";
 
 /**
  * Persist received cookies in memory.
+ * @param bootstrapCookie If set to true, on the first request, if there are no cookies set,
+ * an additional initial HTTP request will be performed to get cookies.
+ * This is often required by session functionalities (like logging-in) to work properly.
+ * The initial request will be performed using GET method with empty body.
  */
-export const persistCookieInterceptor = (): ResponseInterceptor &
-  RequestInterceptor => {
+export const persistCookieInterceptor = (
+  bootstrapCookie: boolean = true
+): ResponseInterceptor & RequestInterceptor => {
+  let bootstrapped = false;
   const cookies = new CookieJar();
 
   return {
     interceptRequest: async (input, config, context) => {
-      const resultCookies = await input.cookies?.serialize() ?? await new CookieJar().serialize();
-      const thisCookies = await cookies.serialize();
-      resultCookies.cookies = resultCookies.cookies.concat(thisCookies.cookies);
-      input.cookies = await CookieJar.deserialize(resultCookies);
+      if(bootstrapCookie && !bootstrapped) {
+        input.method = "get";
+        input.bodyType = "null";
+        input.body = null;
+      }
+      else {
+        const resultCookies =
+          (await input.cookies?.serialize()) ??
+          (await new CookieJar().serialize());
+        const thisCookies = await cookies.serialize();
+        resultCookies.cookies = resultCookies.cookies.concat(thisCookies.cookies);
+        input.cookies = await CookieJar.deserialize(resultCookies);
+      }
     },
 
-    // TODO: We might want to repeat first request if no cookies present - websites often need cookie bootstrap before login
     interceptResponse: async (output, config, context) => {
       if (output.success) {
         // TODO: There might be multiple Set-Cookie headers, but such form of HTTP response is obsolete (https://stackoverflow.com/a/6375214/4272179)
@@ -26,6 +40,11 @@ export const persistCookieInterceptor = (): ResponseInterceptor &
         );
         if (setCookieHeader) {
           await cookies.setCookie(setCookieHeader[1], output.url);
+        }
+
+        if(!bootstrapped && bootstrapCookie) {
+          bootstrapped = true;
+          output.retry = true;
         }
       }
     },
