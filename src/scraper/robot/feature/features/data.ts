d738c330b470a5149e4c95207f1819cf4c2c2d9a
Szymon Jankowski <s.jankowski@fireup.pro>
2021-03-14 10:31:33 +0100

    Document DataFeature

--- src/scraper/robot/feature/features/data.ts
+++ src/scraper/robot/feature/features/data.ts
@@ -1,6 +1,12 @@
 import { ScopeContext } from "../../scope/scope-context";
 import { Feature } from "../feature-class";
 
+/**
+ * DataFeature is a generic feature for reporting and receiving output data from robots.
+ * Reporting data using it requires you to pass a type identifier for the data.
+ *
+ * If you want a customized method and callback for passing data, use {@link defineOutputData}.
+ */
 export class DataFeature extends Feature {
   reportData(scope: ScopeContext, type: string, data: any) {
     this.onDataReceived.invoke(type, data);
@@ -12,6 +18,7 @@ export class DataFeature extends Feature {
 /**
  * Defines a custom feature for reporting output data of a given type.
  * Returns a feature class which can be accessed to report data.
+ * The callback as well as the function to report data supports code completion.
  * @constructor
  */
 export function defineOutputData<T>() {
